5e5742bc09df90b7c0e1c35efe62e71853db552b
mustafa mahmoud <31194057+mostafa-1224@users.noreply.github.com>
2020-02-12 04:37:17 +0200

    Updates

---
+++ precache-manifest.6d185eae672e63c6dc2ab2b69d1cb0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b1e0c176f3eb719bd89b62873431de",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "62c5f2425c6d3610aba8",
+    "url": "/tictactoe/static/css/main.95081d07.chunk.css"
+  },
+  {
+    "revision": "8bb6799253b323e272fb",
+    "url": "/tictactoe/static/js/2.15bd29e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tictactoe/static/js/2.15bd29e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c5f2425c6d3610aba8",
+    "url": "/tictactoe/static/js/main.2fda5828.chunk.js"
+  },
+  {
+    "revision": "c23fd2ecce1bd2ac20fe",
+    "url": "/tictactoe/static/js/runtime-main.48aadb81.js"
+  }
+]);
